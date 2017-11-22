d01dce026d43a13d439cc579707fcc3d53fd0587
Kevin Silberblatt <ksilberblatt@cox.net>
2017-11-22 09:56:58 -0800

    Add user and application controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,8 @@
 class Api::UsersController < ApplicationController
   def create
+    user = User.find_by_credentials(user_params[:email], user_params[:password])
+    if user.save!
+      
   end
 
   private
